a5b14c24d4635f60fa4ca5145df029baee7fd099
Tdrice <ted.barvaut@hotmail.com>
2024-02-02 09:43:21 -0400

    Ajout Bouton et signet map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css'; // Assurez-vous d'importer le CSS
-import MapComponent from './MapComponent'; // Importez le composant de la carte
+import './App.css';
+import MapComponent from './MapComponent';
 
 function App() {
+  const initializeData = () => {
+    console.log('Initialisation des données...');
+    // Ajoutez ici votre logique pour initialiser les données, comme une requête API
+  };
+
   return (
     <div className="App">
+      <button className="initialization-btn" onClick={initializeData}>Initialiser les Données</button>
       <MapComponent />
     </div>
   );
--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; // Importez le CSS de Leaflet
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-const MapComponent = () => {
-  const position = [16.265, -61.551]; // Coordonnées centrées sur la Guadeloupe
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+function AddMarkerToClick() {
+  const [markers, setMarkers] = useState([]);
+
+  useMapEvents({
+    click(e) {
+      const newMarker = e.latlng;
+      setMarkers([...markers, newMarker]);
+    },
+  });
 
   return (
-    <MapContainer center={position} zoom={10} style={{ height: "100vh", width: "100vw" }}>
+    <>
+      {markers.map((position, idx) =>
+        <Marker key={idx} position={position}>
+          <Popup>
+            <div className="form-popup">
+              <form>
+                <div>
+                  <label>Nom du lieu:</label>
+                  <input type="text" placeholder="Entrez un nom" />
+                </div>
+                <div>
+                  <label>Description:</label>
+                  <textarea placeholder="Entrez une description"></textarea>
+                </div>
+                <button type="button">Sauvegarder</button>
+              </form>
+            </div>
+          </Popup>
+        </Marker>
+      )}
+    </>
+  );
+}
+
+const MapComponent = () => {
+  return (
+    <MapContainer center={[16.265, -61.551]} zoom={10} style={{ height: "100vh", width: "100vw" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          Guadeloupe
-        </Popup>
-      </Marker>
+      <AddMarkerToClick />
     </MapContainer>
   );
 };
